2864eca4418d00c7c64b648284ee271041478df7
gsmlg <me@gsmlg.org>
2019-03-20 16:24:54 +0800

    update

---
+++ create.sh
@@ -0,0 +1,13 @@
+docker build -f Dockerfile-jenkins-master -t gsmlg/jenkins-master .
+docker push gsmlg/jenkins-master
+
+docker build -f Dockerfile-jenkins-slave-jnlp1 -t gsmlg/jenkins-slave-jnlp1 .
+docker push gsmlg/jenkins-slave-jnlp1
+
+docker build -f Dockerfile-jenkins-slave-jnlp2 -t gsmlg/jenkins-slave-jnlp2 .
+docker push gsmlg/jenkins-slave-jnlp2
+
+docker build -f Dockerfile-jenkins-slave-jnlp-nodejs -t gsmlg/jenkins-slave-jnlp-nodejs .
+docker push gsmlg/jenkins-slave-jnlp-nodejs 
+
+
