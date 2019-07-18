bb1b80c147e5fd7bcc13bbd6b263dab6e79fbe3f
gsmlg <me@gsmlg.org>
2019-07-18 10:54:26 +0800

    update create.sh

--- create.sh
+++ create.sh
@@ -1,14 +1,17 @@
-docker build -f Dockerfile-jenkins-master -t gsmlg/jenkins-master .
-docker push gsmlg/jenkins-master
+docker pull jenkins/jenkins:lts
+docker pull jenkins/jnlp-slave:latest
 
-docker build -f Dockerfile-jenkins-slave-jnlp1 -t gsmlg/jenkins-slave-jnlp1 .
-docker push gsmlg/jenkins-slave-jnlp1
+docker build -f Dockerfile-jenkins-master -t gsmlg/jenkins-master:latest .
+docker push gsmlg/jenkins-master:latest
 
-docker build -f Dockerfile-jenkins-slave-jnlp2 -t gsmlg/jenkins-slave-jnlp2 .
-docker push gsmlg/jenkins-slave-jnlp2
+#docker build -f Dockerfile-jenkins-slave-jnlp1 -t gsmlg/jenkins-slave-jnlp1:latest .
+#docker push gsmlg/jenkins-slave-jnlp1:latest 
 
-docker build -f Dockerfile-jenkins-slave-jnlp-nodejs -t gsmlg/jenkins-slave-jnlp-nodejs .
-docker push gsmlg/jenkins-slave-jnlp-nodejs
+#docker build -f Dockerfile-jenkins-slave-jnlp2 -t gsmlg/jenkins-slave-jnlp2:latest .
+#docker push gsmlg/jenkins-slave-jnlp2:latest 
 
-docker build -f Dockerfile-jenkins-slave-jnlp-dind -t gsmlg/jenkins-slave-jnlp-dind .
-docker push gsmlg/jenkins-slave-jnlp-dind
+docker build -f Dockerfile-jenkins-slave-jnlp-nodejs -t gsmlg/jenkins-slave-jnlp-nodejs:latest .
+docker push gsmlg/jenkins-slave-jnlp-nodejs:latest 
+
+docker build -f Dockerfile-jenkins-slave-jnlp-dind -t gsmlg/jenkins-slave-jnlp-dind:latest .
+docker push gsmlg/jenkins-slave-jnlp-dind:latest 
